e79f13a844ae7bfac587288d57abef26b068e1a2
Ryan <ryanrutan@live.com>
2023-01-30 20:28:31 -0500

    Menu etc

--- script.js
+++ script.js
@@ -1,12 +1,30 @@
 const getQuote = {
-	method: 'GET',
+	method: "GET",
 	headers: {
-		'X-RapidAPI-Key': '6e3e7feab5mshcb53d970957352dp1303bejsnf471bf3638d9',
-		'X-RapidAPI-Host': 'anime-quotes1.p.rapidapi.com'
-	}
+		"X-RapidAPI-Key": "6e3e7feab5mshcb53d970957352dp1303bejsnf471bf3638d9",
+		"X-RapidAPI-Host": "anime-quotes1.p.rapidapi.com",
+	},
 };
 
-fetch('https://anime-quotes1.p.rapidapi.com/api/quotes/character?name=Goku', getQuote)
-	.then(response => response.json())
-	.then(response => console.log(response))
-	.catch(err => console.error(err));
+fetch(
+	"https://anime-quotes1.p.rapidapi.com/api/quotes/character?name=Goku",
+	getQuote
+)
+	.then((response) => response.json())
+	.then((response) => console.log(response))
+	.catch((err) => console.error(err));
+
+
+$(document).ready(function () {
+	// JQUERY NAV TOGGLE
+	$("#menu").bind("click", function (event) {
+		$("#mainnav ul").slideToggle();
+	});
+
+	$(window).resize(function () {
+		var w = $(window).width();
+		if (w > 768) {
+			$("#mainnav ul").removeAttr("style");
+		}
+	});
+});
