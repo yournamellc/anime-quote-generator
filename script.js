a1bd951d1efa492116217a8b0a6d3b26bafbe2cf
colebrazeal <cbrazeal17@gmail.com>
2023-02-14 15:21:51 -0500

    ghoul page and death note page complete

--- script.js
+++ script.js
@@ -1,19 +1,38 @@
-const getQuote = {
-	method: "GET",
-	headers: {
-		"X-RapidAPI-Key": "6e3e7feab5mshcb53d970957352dp1303bejsnf471bf3638d9",
-		"X-RapidAPI-Host": "anime-quotes1.p.rapidapi.com",
-	},
-};
+// ------------------------NARUTO QUOTE FUNCTION-------------------------
 
-fetch(
-	"https://anime-quotes1.p.rapidapi.com/api/quotes/character?name=Goku",
-	getQuote
-)
-	.then((response) => response.json())
-	.then((response) => console.log(response))
-	.catch((err) => console.error(err));
 
+// ------------------TOKYO GHOUL QUOTE FUNCTION------------------------
+
+const button = document.querySelector("#kaneki-button");
+if (button) {
+	button.addEventListener("click", function () {
+		fetch("json/ghoul.json")
+		.then((response) => response.json())
+		.then((response) => { console.log(response)
+			let animeData = response[Math.floor(Math.random() * response.length)];
+			document.getElementById("quote").innerHTML = animeData.quote;
+			document.getElementById("character").innerHTML = animeData.character;
+		});
+		 });
+}
+
+// ------------------DEATH NOTE QUOTE FUNCTION--------------------
+
+const ryuk = document.querySelector("#deathnote-button");
+if (ryuk) {
+	ryuk.addEventListener("click", function () {
+		fetch("json/ryuk.json")
+		.then((response) => response.json())
+		.then((response) => { console.log(response)
+			let ryukData = response[Math.floor(Math.random() * response.length)];
+			document.getElementById("quote").innerHTML = ryukData.quote;
+			document.getElementById("character").innerHTML = ryukData.character;
+		});
+		 });
+
+}
+
+// ANY API FUNCTION BELOW THIS NAV FUNCTION WONT WORK FOR SOME REASON SO KEEP THIS AT THE BOTTOM
 
 $(document).ready(function () {
 	// JQUERY NAV TOGGLE
@@ -28,19 +47,3 @@ $(document).ready(function () {
 		}
 	});
 });
-
-
-// TOKYO GHOUL QUOTE FUNCTION
-
-const ghoul = {
-	method: 'GET',
-	headers: {
-		'X-RapidAPI-Key': '6e3e7feab5mshcb53d970957352dp1303bejsnf471bf3638d9',
-		'X-RapidAPI-Host': 'anime-quotes1.p.rapidapi.com'
-	}
-};
-
-fetch('https://anime-quotes1.p.rapidapi.com/api/quotes/anime?title=tokyo%20ghoul', ghoul)
-	.then(response => response.json())
-	.then(response => console.log(response))
-	.catch(err => console.error(err));
