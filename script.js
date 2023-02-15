41747328b287b41c6fb032737f8dacb6345d77aa
colebrazeal <cbrazeal17@gmail.com>
2023-02-15 11:51:04 -0500

    ghoul button

--- script.js
+++ script.js
@@ -1,52 +1,3 @@
-
-const getQuote = {
-	method: "GET",
-	headers: {
-		"X-RapidAPI-Key": "6e3e7feab5mshcb53d970957352dp1303bejsnf471bf3638d9",
-		"X-RapidAPI-Host": "anime-quotes1.p.rapidapi.com",
-	},
-};
-
-// fetch(
-// 	"https://anime-quotes1.p.rapidapi.com/api/quotes/character?name=Goku",
-// 	getQuote
-// )
-// 	.then((response) => response.json())
-// 	.then((response) => console.log(response))
-// 	.catch((err) => console.error(err));
-
-// $(document).ready(function () {
-// 	// JQUERY NAV TOGGLE
-// 	$("#menu").bind("click", function (event) {
-// 		$("#mainnav ul").slideToggle();
-// 	});
-
-// 	$(window).resize(function () {
-// 		var w = $(window).width();
-// 		if (w > 768) {
-// 			$("#mainnav ul").removeAttr("style");
-// 		}
-// 	});
-// });
-
-// TOKYO GHOUL QUOTE FUNCTION
-
-const ghoul = {
-	method: "GET",
-	headers: {
-		"X-RapidAPI-Key": "6e3e7feab5mshcb53d970957352dp1303bejsnf471bf3638d9",
-		"X-RapidAPI-Host": "anime-quotes1.p.rapidapi.com",
-	},
-};
-
-// fetch(
-// 	"https://anime-quotes1.p.rapidapi.com/api/quotes/anime?title=tokyo%20ghoul",
-// 	ghoul
-// )
-// 	.then((response) => response.json())
-// 	.then((response) => console.log(response))
-// 	.catch((err) => console.error(err));
-
 // ************************** naruto **************************
 
 const button = document.querySelector("#naruto-button");
@@ -78,14 +29,11 @@ if (titanButton) {
 	});
 }
 
-// ------------------------NARUTO QUOTE FUNCTION-------------------------
-
-
 // ------------------TOKYO GHOUL QUOTE FUNCTION------------------------
 
-const button = document.querySelector("#kaneki-button");
-if (button) {
-	button.addEventListener("click", function () {
+const kaneki = document.querySelector("#kaneki-button");
+if (kaneki) {
+	kaneki.addEventListener("click", function () {
 		fetch("json/ghoul.json")
 		.then((response) => response.json())
 		.then((response) => { console.log(response)
