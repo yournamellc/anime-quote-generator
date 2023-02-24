a1341052620c302984432582909e3510494dc206
colebrazeal <cbrazeal17@gmail.com>
2023-02-24 15:28:48 -0500

    new function and quotes

--- script.js
+++ script.js
@@ -1,30 +1,49 @@
 // ----------------GLOBAL QUOTE & CHARACTER FUNCTIONS----------------------------------
 
-let currentQuote;
+// let currentQuote = null;
 
-let quoteGenerator = (response) => {
-	let newQuote = response[Math.floor(Math.random() * response.length)];
-	while (newQuote === currentQuote) {
-		newQuote = response[Math.floor(Math.random() * response.length)];
-		currentQuote = newQuote;
-		console.log(newQuote);
-		return newQuote;
-	}
-};
+// let quoteGenerator = (response) => {
+// 	let newQuote = response[Math.floor(Math.random() * response.length)];
+// 	while (newQuote === currentQuote) {
+// 		newQuote = response[Math.floor(Math.random() * response.length)];
+// 	}
+// 	currentQuote = newQuote;
+// 	return newQuote;
+// };
+
+var prevIndex = -1;
+function quoteGenerator(response) {
+    // Get the number of quotes in the JSON object    
+	var numQuotes = response.length;
+    // Generate a random index between 0 and numQuotes - 1, but not the same as prevIndex    
+	var randomIndex;
+    do {
+        randomIndex = Math.floor(Math.random() * numQuotes);
+    } while (randomIndex === prevIndex);
+	console.log(prevIndex)
+    prevIndex = randomIndex;
+    // Retrieve the quote and character at the random index    
+	var quote = response[randomIndex].quote;
+    var character = response[randomIndex].character;
+    // Return an object with the quote and character    
+	return { quote: quote, character: character };
+}
 
 // ************************** naruto **************************
 
 const button = document.querySelector("#naruto-button");
 if (button) {
-	button.addEventListener("click", function () {
-		fetch("json/naruto.json")
-			.then((response) => response.json())
-			.then((response) => {
-				let x = quoteGenerator(response);
-				document.getElementById("quote").innerHTML =  x["quote"];
-			})
-			.catch((err) => console.error(err));
-	});
+    button.addEventListener("click", async function () {
+        try {
+            const response = await fetch("json/ghoul.json");
+            const data = await response.json();
+            const { quote, character } = quoteGenerator(data);
+            // document.getElementById("character").innerHTML = character;
+            document.getElementById("quote").innerHTML = quote;
+        } catch (error) {
+            console.error(error);
+        }
+    });
 }
 
 // ************************** titan **************************
@@ -32,15 +51,17 @@ if (button) {
 const titanButton = document.querySelector("#titan-button");
 
 if (titanButton) {
-	titanButton.addEventListener("click", function () {
-		fetch("json/titan.json")
-			.then((response) => response.json())
-			.then((response) => {
-				let x = quoteGenerator(response);
-				document.getElementById("quote").innerHTML =  x["quote"];
-			})
-			.catch((err) => console.error(err));
-	});
+    titanButton.addEventListener("click", async function () {
+        try {
+            const response = await fetch("json/ghoul.json");
+            const data = await response.json();
+            const { quote, character } = quoteGenerator(data);
+            // document.getElementById("character").innerHTML = character;
+            document.getElementById("quote").innerHTML = quote;
+        } catch (error) {
+            console.error(error);
+        }
+    });
 }
 
 // ------------------TOKYO GHOUL QUOTE FUNCTION------------------------
@@ -48,45 +69,51 @@ if (titanButton) {
 const kaneki = document.querySelector("#kaneki-button");
 
 if (kaneki) {
-	kaneki.addEventListener("click", function () {
-		fetch("json/ghoul.json")
-			.then((response) => response.json())
-			.then((response) => {
-				let x = quoteGenerator(response);
-				document.getElementById("quote").innerHTML = x["quote"];
-				document.getElementById("character").innerHTML =  x["character"];
-			});
-	});
+    kaneki.addEventListener("click", async function () {
+        try {
+            const response = await fetch("json/ghoul.json");
+            const data = await response.json();
+            const { quote, character } = quoteGenerator(data);
+            document.getElementById("character").innerHTML = character;
+            document.getElementById("quote").innerHTML = quote;
+        } catch (error) {
+            console.error(error);
+        }
+    });
 }
 
 // ------------------DEATH NOTE QUOTE FUNCTION--------------------
 
 const ryuk = document.querySelector("#deathnote-button");
 if (ryuk) {
-	ryuk.addEventListener("click", function () {
-		fetch("json/ryuk.json")
-			.then((response) => response.json())
-			.then((response) => {
-				let x = quoteGenerator(response);
-				document.getElementById("quote").innerHTML = x["quote"];
-				document.getElementById("character").innerHTML =  x["character"];
-			});
-	});
+    ryuk.addEventListener("click", async function () {
+        try {
+            const response = await fetch("json/ghoul.json");
+            const data = await response.json();
+            const { quote, character } = quoteGenerator(data);
+            document.getElementById("character").innerHTML = character;
+            document.getElementById("quote").innerHTML = quote;
+        } catch (error) {
+            console.error(error);
+        }
+    });
 }
 
 // ---------------------JUJUTSU KAISEN FUNCTION-----------------------------
 
 const jujutsu = document.querySelector("#kaisen-button");
 if (jujutsu) {
-	jujutsu.addEventListener("click", function () {
-		fetch("json/jujutsu.json")
-			.then((response) => response.json())
-			.then((response) => {
-				let x = quoteGenerator(response);
-				document.getElementById("quote").innerHTML = x["quote"];
-				document.getElementById("character").innerHTML =  x["character"];
-			});
-	});
+    jujutsu.addEventListener("click", async function () {
+        try {
+            const response = await fetch("json/ghoul.json");
+            const data = await response.json();
+            const { quote, character } = quoteGenerator(data);
+            document.getElementById("character").innerHTML = character;
+            document.getElementById("quote").innerHTML = quote;
+        } catch (error) {
+            console.error(error);
+        }
+    });
 }
 
 // ANY API FUNCTION BELOW THIS NAV FUNCTION WONT WORK FOR SOME REASON SO KEEP THIS AT THE BOTTOM
